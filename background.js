98bf9674153b5250eff8b246afce4f97b148151d
Ravindu <ravinduwickramasinghe.x@gmail.com>
2024-05-21 19:07:30 +0530

    Update background.js
    Update the default blocking URL (facebook.com)

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.url && changeInfo.url.startsWith("https://www.facebook.com")) {
+  if (changeInfo.url && changeInfo.url.startsWith("https://web.facebook.com/")) {
     chrome.tabs.update(tabId, { url: "index.html" });
   }
 });
